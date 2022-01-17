e209fc4611e81c092db7192999e6c8b1bea7201c
Ade Yudha Pratama <ade@dheiro.com>
2022-01-17 15:11:43 +0700

    setup tailwindcss & font

---
+++ pages/ui.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function UI() {
+  return (
+    <div className="w-full h-screen bg-[#EEF0F8] flex items-center justify-center">
+      <h1>Metronic Tailwind Version</h1>
+    </div>
+  );
+}
